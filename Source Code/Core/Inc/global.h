69c445cd06f74c7a152e047fd1749ab9117f2355
nabicold03 <binh.vunabi2509@hcmut.edu.vn>
2023-12-06 09:41:05 +0700

    Add global.h and global.c files, initialized some definition values (not complete yet!)

---
+++ Source Code/Core/Inc/global.h
@@ -0,0 +1,26 @@
+/*
+ * global.h
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: Admin
+ */
+
+#ifndef INC_GLOBAL_H_
+#define INC_GLOBAL_H_
+
+#include<stdio.h>
+#include<main.h>
+
+#define INIT			1
+#define RED-GREEN		2
+#define RED-YELLOW		3
+#define GREEN-RED		4
+#define YELLOW-RED		5
+
+#define MAN-RED			6
+#define MAN-YELLOW		7
+#define MAN-GREEN		8
+
+
+
+#endif /* INC_GLOBAL_H_ */
---
+++ Source Code/Core/Src/global.c
@@ -0,0 +1,9 @@
+/*
+ * global.c
+ *
+ *  Created on: Dec 6, 2023
+ *      Author: Admin
+ */
+
+#include"global.h"
+
